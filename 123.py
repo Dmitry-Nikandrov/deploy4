3b4574826dfb0c29a980a741ef739fd5842f8fa3
Dmitry <dnikandrov@yandex.ru>
2025-03-13 11:27:25 +0300

    models

---
+++ 123.py
@@ -0,0 +1,3 @@
+from django.core.management.utils import get_random_secret_key
+# print new random secret key
+print(get_random_secret_key())
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=150,verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание')
+    image = models.ImageField(upload_to='photos/',verbose_name='изображение')
+    category = models.CharField(max_length=150,verbose_name='категория')
+    price = models.IntegerField(max_length=10, verbose_name='цена за покупку')
+    created_at = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'продукт {self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['price']
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание')
+
